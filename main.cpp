3a7142713102e3ab9a5060557de4f4673d423c21
juan Pablo Sanchez Gaitan <Juansanchezg@javeriana.edu.co>
2021-09-07 12:18:58 -0500

    añadiendo nuevos condicionales y comentarios a las lineas

--- main.cpp
+++ main.cpp
@@ -17,8 +17,6 @@ int main()
     setlocale(LC_CTYPE, "Spanish");
 
     string Entrada;
-    string Comando;
-    string Auxiliar;
     list<divipola> DiviLista; // se crea la lista
     fstream fout;             // lectura de los archivos
     string linea;
@@ -35,7 +33,7 @@ int main()
         //cin.ignore();
         getline(cin, Entrada);
 
-        // Vector of string to save tokens
+        // Vector para guardar los comandos de entrada
         vector<string> tokens;
 
         // stringstream class check1
@@ -43,13 +41,13 @@ int main()
 
         string intermediate;
 
-        // Tokenizing w.r.t. space ' '
+        // Tokenizando la entrada por los espacios' '
         while (getline(check1, intermediate, ' '))
         {
             tokens.push_back(intermediate);
         }
 
-        // Printing the token vector
+        // Buscando si el comando ingresado esta en el menu de comandos
         for (int i = 0; i < tokens.size(); i++)
         {
             if (tokens[i] == "carga_divipola")
@@ -97,13 +95,13 @@ int main()
             }
             else if (tokens[i] == "listar_municipios")
             {
-                codigodepto = tokens[i + 1];
+                codigodepto = tokens[i + 1]; // aca recoge el valor del codigo ejemplo listar_municipios 45 este recoge el 45 para luego ser buscado
                 cout << codigodepto << endl;
                 comandoEncontrado = true;
             }
             else if (tokens[i] == "listar_poblaciones ")
             {
-                codigoMunicipio = tokens[i + 1];
+                codigoMunicipio = tokens[i + 1]; // aca recoge el valor del codigo ejemplo listar_poblaciones 45 este recoge el 45 para luego ser buscado
                 comandoEncontrado = true;
             }
             else if (tokens[i] == "info_sumaria ")
@@ -125,19 +123,19 @@ int main()
             }
             else if (tokens[i] == "esta_en_sistema")
             {
-                codigoMunicipio = tokens[i + 1];
+                codigoMunicipio = tokens[i + 1]; // aca recoge el valor del codigo ejemplo esta_en_sistema 45 este recoge el 45 para luego ser buscado
                 comandoEncontrado = true;
             }
             else if (tokens[i] == "Ayuda")
             {
                 cout << endl;
-                cout << "carga_divipola nombre_archivo          ... " << '\t' << "Carga en una estructura de datos en memoria el contenido del archivo identificado por nombre_archivo." << endl;
-                cout << "listar_departamentos                   ... " << '\t' << "Imprime en n l�neas (una para cada departamento) la informaci�n b�sica del departamento que se carg� de la Divipola." << endl;
-                cout << "listar_municipios codigo_depto         ... " << '\t' << "Imprime en n l�neas (una para cada municipio) la informaci�n b�sica del departamento que se carg� de la Divipola." << endl;
-                cout << "listar_poblaciones codigo_municipio    ... " << '\t' << "Imprime en n l�neas (una para cada municipio) la informaci�n b�sica del departamento que se carg� de la Divipola." << endl;
-                cout << "info_sumaria codigo_depto              ... " << '\t' << "Imprime en una l�nea la informaci�n b�sica del nombre del departamento que corresponde al c�digo dado como par�metro junto con la cantidad de municipios y centros poblados que lo conforman" << endl;
-                cout << "carga_SC nombre_archivo                ... " << '\t' << "Carga en una o m�s estructuras de datos en memoria el contenido del archivo identificado por nombre_archivo con la informaci�n b�sica para representar el sistema de ciudades." << endl;
-                cout << "esta_en_sistema codigo_municipio       ... " << '\t' << "Determina si un municipio, con c�digo dado por el usuario, existe dentro de las municipios definidos en el Sistema de Ciudades y cargados desde el archivo correspondiente." << endl;
+                cout << "carga_divipola DIVIPOLA_CentrosPoblados          ... " << '\t' << "Carga en una estructura de datos en memoria el contenido del archivo identificado por nombre_archivo." << endl;
+                cout << "listar_departamentos                   ... " << '\t' << "Imprime en n lineas (una para cada departamento) la informacion basica del departamento que se carg� de la Divipola." << endl;
+                cout << "listar_municipios codigo_depto         ... " << '\t' << "Imprime en n lineas (una para cada municipio) la informacion basica del departamento que se carg� de la Divipola." << endl;
+                cout << "listar_poblaciones codigo_municipio    ... " << '\t' << "Imprime en n lineas (una para cada municipio) la informacion basica del departamento que se carg� de la Divipola." << endl;
+                cout << "info_sumaria codigo_depto              ... " << '\t' << "Imprime en una linea la informacion basica del nombre del departamento que corresponde al c�digo dado como par�metro junto con la cantidad de municipios y centros poblados que lo conforman" << endl;
+                cout << "carga_SC Datos-ICM-2019                ... " << '\t' << "Carga en una o mas estructuras de datos en memoria el contenido del archivo identificado por nombre_archivo con la informaci�n b�sica para representar el sistema de ciudades." << endl;
+                cout << "esta_en_sistema codigo_municipio       ... " << '\t' << "Determina si un municipio, con codigo dado por el usuario, existe dentro de las municipios definidos en el Sistema de Ciudades y cargados desde el archivo correspondiente." << endl;
                 /* cout << "" << endl;
             cout << "" << endl;
             cout << "" << endl;
@@ -149,7 +147,7 @@ int main()
             cout << "" << endl;
             cout << "" << endl;
             cout << "" << endl;*/
-                cout << "Salir                                  ... Termina la ejecuci�n de la aplicaci�n." << endl;
+                cout << "Salir                                  ... Termina la ejecucion de la aplicacion." << endl;
                 cout << endl;
                 comandoEncontrado = true;
             }
@@ -158,9 +156,10 @@ int main()
                 Salir = true;
                 comandoEncontrado = true;
             }
-            if (!comandoEncontrado)
+            if (!comandoEncontrado) // si el comando no se encuentra el arroja este error de comando no encontrado
             {
                 cout << "Comando no identificado" << endl;
+                cout << "Por favor ingrese ayuda para tener mas informacion de los comandos" << endl;
             }
         }
     } while (!Salir);
